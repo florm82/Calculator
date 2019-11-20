9957b98a158b1b30823f9d22115d288bf20611b6
Flor Nazari <florm82@gmail.com>
2019-11-19 23:43:44 -0800

    add a variable named BMI call a function

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,9 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = divided(number1, number2)
 alert(result);
+
+var BMI = function(number1, number2) {return number1 / number2;};
+var number1 = parseInt(prompt("your weight in kg"));
+var number2 = parseInt(prompt("your height in metres"));
+var result = divided(number1, number2)
+alert(result);
