07256c38b0046bd1f52b431f433d2b153e4f15c6
Flor Nazari <florm82@gmail.com>
2019-12-04 17:22:33 -0800

    add new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,24 @@
+
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {return number1 + number2;};
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2)
-alert(result);
+
 
 var multiply = function(number1, number2) {return number1 * number2;};
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2)
-alert(result);
+
 
 var subtract = function(number1, number2) {return number1 - number2;};
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2)
-alert(result);
+
 
 var divided = function(number1, number2) {return number1 / number2;};
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divided(number1, number2)
-alert(result);
 
-var BMI = function(number1, number2) {return number1 / number2;};
-var number1 = parseInt(prompt("your weight in kg"));
-var number2 = parseInt(prompt("your height in metres"));
-var result = divided(number1, number2)
-alert(result);
+// Everything below this line is user interface (or front-end) logic:
 
-
-var cToF = function(cTemp * 9 / 5 +32)
-var cTemp = celesius;
-var cToFahr = cTemp * 9 / 5 + 32;
-var result = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
-alert(result);
-
-var sample_regex = /epicodus/gi
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
