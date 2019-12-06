4f4db9d923569fdf370d6e1bcfa4bba010fa1c7b
Flor Nazari <florm82@gmail.com>
2019-12-06 12:02:20 -0800

    add some new functions in interface logic part

--- js/scripts.js
+++ js/scripts.js
@@ -15,41 +15,12 @@ var divided = function(number1, number2) {return number1 / number2;};
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function() {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
     var result = add(number1, number2);
     $("#output").text(result);
   });
-});
-
-$(document).ready(function() {
- $("form#subtract").submit(function(event) {
-   event.preventDefault();
-   var number1 = parseInt($("#add1").val());
-   var number2 = parseInt($("#add2").val());
-   var result = subtract(number1, number2);
-   $("#output").text(result);
- });
-});
-
-$(document).ready(function() {
- $("form#multiply").submit(function(event) {
-   event.preventDefault();
-   var number1 = parseInt($("#add1").val());
-   var number2 = parseInt($("#add2").val());
-   var result = multiply(number1, number2);
-   $("#output").text(result);
- });
-});
-
-$(document).ready(function() {
- $("form#divided").submit(function(event) {
-   event.preventDefault();
-   var number1 = parseInt($("#add1").val());
-   var number2 = parseInt($("#add2").val());
-   var result = divided(number1, number2);
-   $("#output").text(result);
- });
 });
